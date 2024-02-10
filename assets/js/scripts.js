a94d2f07520f779c8d65e395f961ec3285346dc0
Tomas <tomastapia@gmail.com>
2024-02-09 23:22:34 -0300

    test js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,13 @@
-alert("Esta es una Alerta");
 
+let numeroA = 3;
+let numeroB = 5;
+
+let resultado = numeroA + numeroB;
+
+console.log(resultado);
+
+if(resultado === 8) {
+    console.log("El resultado es correcto");
+} else {
+    console.log("El resultado es incorrecto");
+}
